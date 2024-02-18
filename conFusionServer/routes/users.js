1729d2cc029e8819f8fd9c5fe0f7779f60254d21
DDJar <syquocvietoppo@gmail.com>
2024-02-18 17:56:56 +0700

    Test signup v2

--- conFusionServer/routes/users.js
+++ conFusionServer/routes/users.js
@@ -19,28 +19,31 @@ router.get('/', authenticate.verifyOrdinaryUser, authenticate.verifyAdmin, (req,
     .catch((err) => next(err));
 });
 router.post('/signup', (req, res, next) => {
-  User.register(new User({ username: req.body.username }), req.body.password)
-    .then((user) => {
-      if (req.body.firstname) {
-        user.firstname = req.body.firstname;
-      }
-      if (req.body.lastname) {
-        user.lastname = req.body.lastname;
-      }
-      return user.save();
-    })
-    .then((newUser => {
-      passport.authenticate('local')(req, res, () => {
-        var token = authenticate.getToken({ _id: req.user._id });
-        res.statusCode = 200;
+  User.register(new User({ username: req.body.username }),
+    req.body.password, (err, user) => {
+      if (err) {
+        res.statusCode = 500;
         res.setHeader('Content-Type', 'application/json');
-        res.json({ success: true, token: token, status: 'Registration Successful!' });
-      });
-    }))
-    .catch((err) => {
-      res.statusCode = 500;
-      res.setHeader('Content-Type', 'application/json');
-      res.json({ err: err.message });
+        res.json({ err: err });
+      }
+      else {
+        if (req.body.firstname)
+          user.firstname = req.body.firstname;
+        if (req.body.lastname)
+          user.lastname = req.body.lastname;
+        user.save().then(user => {
+          passport.authenticate('local')(req, res, () => {
+            var token = authenticate.getToken({ _id: req.user._id });
+            res.statusCode = 200;
+            res.setHeader('Content-Type', 'application/json');
+            res.json({ success: true, token: token, status: 'Registration Successful!' });
+          });
+        }).catch(err => {
+          res.statusCode = 500;
+          res.setHeader('Content-Type', 'application/json');
+          res.json({ err: err });
+        });;
+      }
     });
 });
 router.post('/login', passport.authenticate('local'), (req, res) => {
